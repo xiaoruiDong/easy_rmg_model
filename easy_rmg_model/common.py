68910c07cca284177a324ec369f85b351b36ba8f
Xiaorui Dong <xiaorui@mit.edu>
2020-05-07 00:50:12 -0400

    Add `get_files_by_regex` to common

--- easy_rmg_model/common.py
+++ easy_rmg_model/common.py
@@ -2,9 +2,11 @@
 # encoding: utf-8
 
 import os
+import re
 
 
-def regularize_path(path: str):
+
+def regularize_path(path: str) -> str:
     """
     Regularize the path.
     If the path has a initial component of ~ or ~user, it will be replaced by home directory
@@ -17,3 +19,22 @@ def regularize_path(path: str):
         return os.path.expandvars(os.path.expanduser(path))
     else:
         return os.path.abspath(os.path.expandvars(path))
+
+
+def get_files_by_regex(path: str, regex: str) -> list:
+    """
+    Get all the file paths corresponding the regex given
+
+    Args:
+        path (str): The directory which contains files to be found
+        regex (regex): The regular expression of the search
+
+    Return:
+        file_list (list): A list of file paths
+    """
+    file_list = list()
+    for root, _, files in os.walk(path):
+        for file_name in files:
+            if re.search(regex, file_name):
+                file_list.append(os.path.join(root, file_name))
+    return file_list
