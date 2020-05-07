0b6ec3fbe5efea08bc7fb2dcc81fb69e3a668d88
Xiaorui Dong <xiaorui@mit.edu>
2020-05-07 00:54:13 -0400

    Add `expand_spc_info_by_spc_dict`

--- easy_rmg_model/rmg2arc/species_dict.py
+++ easy_rmg_model/rmg2arc/species_dict.py
@@ -26,3 +26,44 @@ def load_spc_dict(spc_dict: Union[dict, str]) -> dict:
         spc_dict = load_species_dictionary(spc_dict)
     else:
         raise ValueError(f'Invalid species dictionary {spc_dict}')
+
+
+def expand_spc_info_by_spc_dict(spc_info: dict,
+                                spc_dict: Union[str, dict],
+                                spc_aliases: Optional[dict]):
+    """
+    Expand the given spc_info ``dict`` by using species dictionary. The updates are
+    based on the label information.
+
+    Args:
+        spc_info: The species info, which should at least contains label information
+        spc_dict (Union[dict, str]): The path to species dictionary or the
+                                     actual species dictionary
+        spc_dict (Optional[dict]): In case label systems are different, you can provide
+                                   aliases to help convert the names
+
+    Returns:
+        dict: The updated species information
+    """
+
+    spc_dict = load_spc_dict(spc_dict)
+
+    for label, spc in spc_info.items():
+        if spc_aliases:
+            label = spc_aliases[label]
+        try:
+            rmg_spc = spc_dict[label]
+        except KeyError:
+            print(f'Warning: Cannot find the species {spc["label"]} in species dictionary.')
+        else:
+            try:
+                smiles = rmg_spc.molecule[0].to_smiles()
+                charge = rmg_spc.molecule[0].get_net_charge()
+                adjlist = rmg_spc.to_adjacency_list()
+            except:
+                print(f'Warning: Cannot generate geom info for the species {label}.')
+            else:
+                spc.update({'smiles': smiles,
+                            'adjlist': adjlist, 'multiplicity': spc.multiplicity,
+                            'charge': charge, })
+    return spc_dict
