1c0aecc8efffe5541676c3f2735d8029d4fd8075
Xiaorui Dong <xiaorui@mit.edu>
2021-11-10 23:02:56 -0500

    Use RDMC logs as the backend
    Currently, only Gaussian Logs are supported.

--- easy_rmg_model/job/trsh.py
+++ easy_rmg_model/job/trsh.py
@@ -3,11 +3,9 @@
 
 
 from arkane.ess import ess_factory, GaussianLog, MolproLog, OrcaLog, QChemLog, TeraChemLog
+from rdmc.external.gaussian import GaussianLog
 
-from arc.job.trsh import determine_ess_status
-
-from arc.parser import parse_frequencies
-
+LOG_PARSER = {'GaussianLog': GaussianLog}
 
 def determine_convergence(path, job_type, ts=False):
     try:
@@ -15,25 +13,20 @@ def determine_convergence(path, job_type, ts=False):
     except:
         return False
     else:
-        for log_type in [GaussianLog, MolproLog, OrcaLog, QChemLog, TeraChemLog]:
-            if isinstance(log, log_type):
-                software = log_type.__name__.replace('Log', '').lower()
-                break
-    try:
-        done = determine_ess_status(path,
-                                    species_label='',
-                                    job_type=job_type,
-                                    software=software,
-                                    )[0] == 'done'
-    except:
+        try:
+            log = LOG_PARSER[log.__class__.__name__](path)
+        except KeyError:
+            raise NotImplementedError
+
+    if not log.success:
         return False
 
-    if done and job_type in ['optfreq', 'freq', 'composite']:
-        freqs = parse_frequencies(path=path, software=software)
-        if not len(freqs):
-            # Single atom without freq
-            return done
-        neg_freqs = [freq for freq in freqs if freq < 0]
+    if job_type in ['optfreq', 'freq', 'composite']:
+        if not len(log.freqs): # Single atom without freq
+            return True
         if not ts:
-            return done and not len(neg_freqs)
-        return done and len(neg_freqs) == 1
+            return len(log.neg_freqs) == 0
+        else:
+            return len(log.neg_freqs) == 1
+    else:
+        return True
